786f74c91cf951eaa0d3cde01148383c0ae934c3
ShashankMirketa <shashank.patel@mirketa.com>
2026-09-15 16:16:56 +0530

    Fixing LWC helloWorld that caused pipeline failure

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,5 +1,3 @@
 import { LightningElement } from "lwc";
 
-export default class HelloWorld extends LightningElement {
-  message = "Hello Devops1";
-}
+export default class HelloWorld extends LightningElement {}
